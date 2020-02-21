40fe20635f178f4436282ab6855b4a1b200f48e9
mizukick <mizukijan@gmail.com>
2020-02-21 13:09:44 +0900

    ナビゲーションバーの文字色変更 #2

--- TrainingNote/AppDelegate.swift
+++ TrainingNote/AppDelegate.swift
@@ -13,9 +13,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         let defaultsExercise = ["Bench press", "Squat", "Dead lift"]
         UserDefaults.standard.register(defaults: ["exercise": defaultsExercise])
+        // ナビゲーションバーのタイトルの文字色
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.lightText]
         return true
     }
 
